5c30738d0340fb0f8c7f3f700f00808c4b02bb74
Revanth <princechalapathi16@gmail.com>
2019-02-06 07:57:49 +0000

    Import a Build Script

---
+++ build.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/ChalapathiRevanth/Toolchain ~/Toolchain
+export ARCH=arm64
+export CROSS_COMPILE=~/Toolchain/bin/aarch64-linux-android-
+mkdir output
+make -C $(pwd) O=output sakura_defconfig
+make -j32 -C $(pwd) O=output
+git clone https://github.com/Revanth/AnyKernel2 zip
+cp -r output/arch/arm64/boot/Image.gz-dtb zip/
+cd zip
+mv Image.gz-dtb zImage 
+zip -r THUNDERSTORM-Sakura-V1.zip *
