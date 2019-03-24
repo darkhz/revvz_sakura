f369e033c30d1a01e7d7d0bf6fbc8443decb3c9e
Revanth <princechalapathi16@gmail.com>
2019-03-24 15:14:11 +0000

    Update Build Script

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 git clone https://github.com/ChalapathiRevanth/Toolchain ~/Toolchain
 export ARCH=arm64
 export CROSS_COMPILE=~/Toolchain/bin/aarch64-linux-android-
+export KBUILD_BUILD_USER=RevanthTolety
+export KBUILD_BUILD_HOST=RevanthTolety@thunderstorm-buildserver
 mkdir output
 make -C $(pwd) O=output sakura_defconfig
 make -j32 -C $(pwd) O=output
@@ -8,4 +10,4 @@ git clone https://github.com/Revanth/AnyKernel2 zip
 cp -r output/arch/arm64/boot/Image.gz-dtb zip/
 cd zip
 mv Image.gz-dtb zImage 
-zip -r ThunderStorm-Sakura-V1.1.zip *
+zip -r ThunderStorm-Sakura-V2.zip *
