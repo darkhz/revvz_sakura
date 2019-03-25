2c399374dda3572ec9bd77ab0c2dac10a72f3cbe
Revanth <princechalapathi16@gmail.com>
2019-03-25 14:00:18 +0000

    Update Build Host

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ git clone https://github.com/ChalapathiRevanth/Toolchain ~/Toolchain
 export ARCH=arm64
 export CROSS_COMPILE=~/Toolchain/bin/aarch64-linux-android-
 export KBUILD_BUILD_USER=RevanthTolety
-export KBUILD_BUILD_HOST=RevanthTolety@thunderstorm-buildserver
+export KBUILD_BUILD_HOST=thunderstorm-buildserver
 mkdir output
 make -C $(pwd) O=output sakura_defconfig
 make -j32 -C $(pwd) O=output
